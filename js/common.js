16e04da5a8ff89462f4bcecc4a03439f00248596
Valera19900 <kaktuz34@gmail.com>
2014-06-26 17:56:48 +0300

    design-block

--- js/common.js
+++ js/common.js
@@ -7,5 +7,25 @@ head.ready(function() {
 		$(".js-popup").hide();
 	});
 
-	console.log($('body').html());
+	function tab() {
+       $(".js-tab").each(function(){
+         var tab_link = $(this).find("a");
+         var tab_item = $(this).find("li");
+         var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+         tab_cont.hide();
+         tab_item.first().addClass("is-active");
+         $(this).parents(".js-tab-group").find(".js-tab1").show();
+         tab_link.on("click", function() {
+             var index = $(this).attr("href");
+             tab_item.removeClass("is-active");
+             $(this).parent().addClass("is-active");
+             tab_cont.hide();
+             $(this).parents(".js-tab-group").find("."+index).show();
+             return false;
+          });
+       });
+	}
+	tab();
+
+	// console.log($('body').html());
 });
