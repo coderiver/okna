ef61930f54687c22e2538bd52f4aa517567f65e8
akella <akella.a@gmail.com>
2014-06-30 14:44:16 +0300

    help popup

--- js/common.js
+++ js/common.js
@@ -31,22 +31,7 @@ head.ready(function() {
                     }
                 }
             });
-        
-            // $(".section"+n).addClass('is-active').css({'opacity':0}).animate({
-            //     opacity: 1
-            //   }, 1000, function() {
-            //         // Animation complete.
-            //         $(this).addClass('a');
-            //         $('.section.was-active').removeClass('was-active');
-                    
-            //         setTimeout(function(){running = 0;},1000);
-            //         if(n==5){
-            //            if($('.tab-cont.is-shown').length){}else{
-            //             $(".js-tab1").addClass('is-shown');
-            //             }
-            //         }
-            //         $("html, body").animate({ scrollTop: 0 }, "fast");
-            //   });
+
 
         }
         
@@ -60,14 +45,7 @@ head.ready(function() {
         if(cur>1){goto(cur-1);}
     }
 
-    $(window).scroll(function() {
-       if($(window).scrollTop() + $(window).height() == getDocHeight()) {
-           next();
-       }
-       if($(window).scrollTop()==0){
-            prev();
-       }
-    });
+
     //goto(1);
     $('.nav__ul a').click(function(event) {
         n = $(this).attr('href');
@@ -75,16 +53,6 @@ head.ready(function() {
         goto(n);
         return false;
     });
-    // s2 = $(".section2").offset().top;
-    // s3 = $(".section3").offset().top;
-    // s4 = $(".section4").offset().top;
-    // s5 = $(".section5").offset().top;
-    // s6 = $(".section6").offset().top;
-    // s7 = $(".section7").offset().top;
-    // sp = 100;
-    // var lastScrollTop = 0;
-    // var curr = 1;
-    // var curr1 = 1;
     var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
     
 
@@ -107,69 +75,7 @@ head.ready(function() {
         
 
     });
-    $(window).scroll(function(event) {
-        // ttop = $(document).scrollTop();
-        // h = $(window).height();
-        // //console.log(ttop);
-        // if(ttop>s2-sp){$('.section2').addClass('a');}
-        // if(ttop>s3-sp){$('.section3').addClass('a');}
-        // if(ttop>s4-sp){$('.section4').addClass('a');}
-        // if(ttop>s5-sp){
-        //     $('.section5').addClass('a');
-        //     if($('.tab-cont.is-shown').length){}else{
-        //         $(".js-tab1").addClass('is-shown');
-        //     }
-        // }
-        // if(ttop>s6-sp){$('.section6').addClass('a');}
-        // if(ttop>s7-sp){$('.section7').addClass('a');}
 
-
-        // var st = $(this).scrollTop();
-        
-        //    if (st > lastScrollTop){
-        //        // downscroll code
-        //        //console.log('down');
-        //        if(ttop+h -20>s2){curr1 = 2}
-        //        if(ttop+h -20>s3){curr1 = 3}
-        //        if(ttop+h -20>s4){curr1 = 4}
-        //        if(ttop+h -20>s5){curr1 = 5}
-        //        if(ttop+h -20>s6){curr1 = 6}
-        //        if(ttop+h -20>s7){curr1 = 7}
-        //         console.log(curr1);
-        //    } else {
-        //       // upscroll code
-        //         if(ttop<s7){curr1 = 6}
-        //         if(ttop<s6){curr1 = 5}
-        //         if(ttop<s5){curr1 = 4}
-        //         if(ttop<s4){curr1 = 3}
-        //         if(ttop<s3){curr1 = 2}
-        //         if(ttop<s2){curr1 = 1}
-                   
-               
-        //     console.log(curr1);
-        //    }
-        //    if(!$('body').hasClass('running')){
-
-        //              if(curr!=curr1){
-        //                 //$('body').addClass('running');
-        //                 goto(curr1);
-        //                 console.log(curr1);
-        //                 curr = curr1;
-        //            }
-        //    }
-          
-           
-        //    lastScrollTop = st;
-
-        // determin nomer slide po bottom
-
-        // $('.section').each(function(){
-        //     $(this)
-        // })
-
-        //get nomer of current slide of bottom
-        
-    });
 
 
     // first slide, activate
@@ -186,15 +92,11 @@ head.ready(function() {
         //y = 0;
         $('.home__blue').css({'margin-left': x + 'px'});
         $('.home__green').css({'margin-left': x/2 + 'px'});
-        //$('.parallaxpic').css({'margin-top':y/4 + 'px'});
+        $('.parallaxpic').css({'margin-top':y/4 + 'px'});
     });
     
 
-    $('.home__window').mousemove(function(e){
-        var x = -(e.pageX) / 20;
-        var y = -(e.pageY) / 5;
-        $('.parallaxpic').css({'margin-top':y/4 + 'px'});
-    });
+
 
 
 
@@ -267,7 +169,14 @@ head.ready(function() {
     $('.popup__close,.overlay').click(function(event) {
         $('.overlay,.popup').fadeOut();
     });
-
+    $('.help').click(function(event) {
+        $(this).addClass('is-open').children('.help__popup').fadeIn();
+        return false;
+    });
+    $(document).on("click", function(){
+        $('.help').removeClass('is-open');
+        $(".help__popup").fadeOut();
+    });
 
 	// console.log($('body').html());
 });
